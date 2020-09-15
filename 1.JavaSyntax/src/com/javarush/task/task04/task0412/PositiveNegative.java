4c05fe1209b362c0a18a3b5bf2d1c467d00412e1
Justiam <vlavad@tut.by>
2020-09-16 01:10:59 +0300

    tasks

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0412/PositiveNegative.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task04.task0412;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Положительное и отрицательное число
+Ввести с клавиатуры число.
+Если число положительное, то увеличить его в два раза.
+Если число отрицательное, то прибавить единицу.
+Если введенное число равно нулю, необходимо вывести ноль.
+Вывести результат на экран.
+
+*/
+
+public class PositiveNegative {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        int number = Integer.parseInt(bufferedReader.readLine());
+        if (number > 0)
+            number *= 2;
+        else if (number < 0)
+            number += 1;
+        System.out.println(number);
+
+    }
+
+}
